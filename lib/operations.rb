c7a49370143fcf7e389d608a31258ffd030cee12
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-06 19:33:32 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,13 +11,8 @@ end
 
 
 def not_safe?(speed)
-	if speed > 60
-	  return true
-	elsif speed < 40
-	  return true
-	 elsif speed <= 60 && speed >= 40
-	  return false
-	end
+  speed <= 60 && speed >= 40 ? return false : return true
+
 end
 	
 
